c3d00901031157bfa67983f76416d10ab2742b8b
Astig-1982 <astig.mandalian2014@gmail.com>
2020-05-05 16:16:51 +0000

    Created the function for the quick view display

---
+++ assets/scripts/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#quickView').click(function(){
+		let el = $(this).html();
+		if(el == 'BACK') {
+			$('.title').html('<h1 class="display-4">SMART QUIZ</h1><h2 class="lead">The Fun Way To Test Your Knowledge</h2><button id="startQuiz">START QUIZ</button>');
+			$('#quickView').html('READY');
+		} else {
+			$('.title').html('<p class="display-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p><p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>');
+			$('#quickView').html('BACK');
+		};
+	});
+});
