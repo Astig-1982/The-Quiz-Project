20aa6b438db4242cead18bd6baa0a3466278105a
Astig-1982 <astig.mandalian2014@gmail.com>
2020-05-23 20:42:59 +0000

    Modified text to inner html at 'quick view' click event function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     let el = $(this).html();
     if (el == "BACK") {
       $(".title").html(
-        '<h1 class="display-4">SMART QUIZ</h1><h2 class="lead">The Fun Way To Test Your Knowledge</h2><button id="startQuiz">START QUIZ</button>'
+        '<h1 id="main-title" class="uppercase">master quiz</h1><h2>The Quick Way To Test Your Knowledge</h2><button id="startQuiz" class="main-buttons">start quiz</button>'
       );
       $("#quickView").html("QUICK VIEW");
     } else {
       $(".title").html(
-        '<p class="display-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p><p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>'
+        '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p><p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>'
       );
       $("#quickView").html("BACK");
     }
